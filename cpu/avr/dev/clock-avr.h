0e5af9b7f3e3b018ed9fee3e4c6e3d361b7e70ed
Andrew Turner <andrew@fubar.geek.nz>
2009-05-17 21:17:44 +1200

    Setup the clock to provice the tick interrupt

--- cpu/avr/dev/clock-avr.h
+++ cpu/avr/dev/clock-avr.h
@@ -72,6 +72,40 @@
 
 #define AVR_OUTPUT_COMPARE_INT TIMER0_COMPA_vect
 
+#elif defined (__AVR_ATmega328P__)
+#define OCRSetup() \
+  /* Select internal clock */ \
+  ASSR = 0x00; 				  \
+\
+  /* Set counter to zero */   \
+  TCNT0 = 0;				  \
+\
+  /*						  \
+   * Set comparison register: \
+   * Crystal freq. is 16000000,\
+   * pre-scale factor is 1024, i.e. we have 125 "ticks" / sec: \
+   * 16000000 = 1024 * 125 * 125 \
+   */ \
+  OCR0A = 125; \
+\
+  /* 								\
+   * Set timer control register: 	\
+   *  - prescale: 1024 (CS00 - CS02) \
+   *  - counter reset via comparison register (WGM01) \
+   */ 								\
+  TCCR0A =  _BV(WGM01); \
+  TCCR0B =  _BV(CS00) | _BV(CS02); \
+\
+  /* Clear interrupt flag register */ \
+  TIFR0 = 0x00; \
+\
+  /* \
+   * Raise interrupt when value in OCR0 is reached. Note that the \
+   * counter value in TCNT0 is cleared automatically. \
+   */ \
+  TIMSK0 = _BV (OCIE0A);
+
+#define AVR_OUTPUT_COMPARE_INT TIMER0_COMPA_vect
 #else
 #error "Setup CPU in clock-avr.h"
 #endif
